fdabcf58528211286ee6100c902d77cd60fb489f
soulmate <18720720569@163.com>
2019-11-28 14:44:39 +0800

    209/11/28提交

--- src/main/java/org/som/bookshop/entity/Address.java
+++ src/main/java/org/som/bookshop/entity/Address.java
@@ -1,5 +1,7 @@
 package org.som.bookshop.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -11,6 +13,7 @@ import lombok.Data;
 @Data
 @TableName(value = "bs_address")
 public class Address {
+    @TableId(type = IdType.AUTO)
     private Integer id;
     private Integer userId;
     private String province;
@@ -22,4 +25,83 @@ public class Address {
     private String tel;
     private String isDefault;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getDetailAddress() {
+        return detailAddress;
+    }
+
+    public void setDetailAddress(String detailAddress) {
+        this.detailAddress = detailAddress;
+    }
+
+    public String getEmailCode() {
+        return emailCode;
+    }
+
+    public void setEmailCode(String emailCode) {
+        this.emailCode = emailCode;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getIsDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(String isDefault) {
+        this.isDefault = isDefault;
+    }
 }
---
+++ src/main/java/org/som/bookshop/web/AddressController.java
@@ -0,0 +1,34 @@
+package org.som.bookshop.web;
+
+import org.som.bookshop.entity.User;
+import org.som.bookshop.entity.Address;
+import org.som.bookshop.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author : soulmate
+ * @date : 2019/11/27  11:41
+ * @description :
+ */
+@Controller
+@RequestMapping("/address")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    @ResponseBody
+    @RequestMapping("save")
+    public String save(Address address , HttpSession session){
+        User user = (User)session.getAttribute("user");
+        address.setUserId(user.getId());
+        addressService.save(address);
+        return "success";
+    }
+
+}
